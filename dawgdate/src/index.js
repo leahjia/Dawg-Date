6276fd784e1cfa357c60d1079d2158b6f8bf50cd
Leah Jia <yuwenj3@uw.edu>
2022-11-10 11:52:30 -0800

    added simple title

--- dawgdate/src/index.js
+++ dawgdate/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const msgElem = <h1 id="DawgDate" className='title'></h1>
+const msgElem = <h1 id="DawgDate" className='title'>Title</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(msgElem);
