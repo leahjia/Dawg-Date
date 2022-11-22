0c2d019714cc7eb4901a4c1788a4fbe2a2870232
vinh <vinh.q.nguyen0122@gmail.com>
2022-11-22 15:00:13 -0800

    basic component layout

--- dawgdate/src/components/App.js
+++ dawgdate/src/components/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from './Header.js';
 import { ProfileList } from './ProfileList.js';
+import Footer from './Footer.js'
+import LandingPage from './LandingPage.js';
+
 
 export default function App(props) {
-    return (
-      <div>
-        <Header />
-        <ProfileList />
-      </div>
-    )
+    const [currentUrl, setCurrentUrl] = useState('');
+    const pages = ['landing', 'home'];
+    const currentPage = 'landing';
+
+    if (currentPage == pages[0]) {
+      return <LandingPage />
+    } else {
+      return (
+        <div>
+          <Header />
+          <ProfileList />
+          <Footer />
+        </div>
+      )
+    }
   }
   
--- dawgdate/src/index.js
+++ dawgdate/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/App.js';
 
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import './style-project.css';
 
 // const msgElem = <h1 id="DawgDate" className='title'>Title</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
