6b1f3ec23593b2820585db321cb7f7ac6eb87cca
vinh <vinh.q.nguyen0122@gmail.com>
2022-11-22 15:45:45 -0800

    routing setup and icons

--- dawgdate/src/components/App.js
+++ dawgdate/src/components/App.js
@@ -1,26 +1,22 @@
 import React, { useState } from 'react';
+import { Route, Routes, Outlet } from 'react-router-dom';
 
-import { Header } from './Header.js';
-import { ProfileList } from './ProfileList.js';
-import Footer from './Footer.js'
 import LandingPage from './LandingPage.js';
+import HomePage from './HomePage.js';
+import SelfProfilePage from './SelfProfilePage.js';
+import OtherProfilePage from './OtherProfilePage.js';
 
 
 export default function App(props) {
-    const [currentUrl, setCurrentUrl] = useState('');
-    const pages = ['landing', 'home'];
-    const currentPage = 'landing';
 
-    if (currentPage == pages[0]) {
-      return <LandingPage />
-    } else {
-      return (
-        <div>
-          <Header />
-          <ProfileList />
-          <Footer />
-        </div>
-      )
-    }
+  return(
+    <Routes>
+      <Route index element={<LandingPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/profile" element={<SelfProfilePage />} />
+      <Route path="/:username" element={<OtherProfilePage />} />
+    </Routes>
+  )
+
   }
   
--- dawgdate/src/index.js
+++ dawgdate/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App.js';
 
@@ -9,4 +10,8 @@ import './style-project.css';
 
 // const msgElem = <h1 id="DawgDate" className='title'>Title</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
