7f37051697bae7d3a86411dd921d9a3c1b0c0656
eivy1234 <eivy1234@uw.edu>
2022-11-23 14:13:30 -0800

    connections

---
+++ dawgdate/src/components/ConnectionsPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Links } from 'react-router-dom';
+
+import { ProfileList } from "./ProfileList";
+import NavBar from "./NavBar";
+
+export default function ConnectionsPage(props) {
+	const currentUser = props.currentUser;
+
+	const displayedData = props.profileData.filter((userProfile) => {
+		if (currentUser.connections.includes(userProfile.UWNetId)) {
+			return true;
+		} else {
+			return false;
+		}
+	})
+
+	return (
+		<div>
+			<header className="navbar-bg">
+                <NavBar/>
+            </header>
+			<main>
+				<div className="profile-list">
+					<h1>My Connections</h1>
+					<ProfileList profileData={displayedData} currentUser={props.currentUser}/>
+				</div>
+			</main>
+		</div>
+	)
+
+}
