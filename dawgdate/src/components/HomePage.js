90305cf309ccc18dfd297f6cac92f2e3d7cd54fb
Leah Jia <yuwenj3@uw.edu>
2022-11-22 19:16:29 -0800

    eliminate footer dup

--- dawgdate/src/components/HomePage.js
+++ dawgdate/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from './Footer.js';
 import NavBar from './NavBar.js';
 import Search from './Search.js';
 import { ProfileList } from './ProfileList.js';
@@ -6,19 +7,16 @@ import { ProfileList } from './ProfileList.js';
 export default function HomePage(props) {
     return (
         <div>
-			<header className="navbar-bg">
-				<NavBar></NavBar>
-			</header>
-
-			<main>
-				<section className="search-container">
-					<Search></Search>
-				</section>
-				<section className="profile-list">
-					<h1>People You May Know</h1>
-					<ProfileList></ProfileList>
-				</section>
-			</main>
+            <header className="navbar-bg">
+                <NavBar></NavBar>
+            </header>
+            <main>
+                <Search />
+                <section className="profile-list">
+                    <h1>People You May Know</h1>
+                    <ProfileList></ProfileList>
+                </section>
+            </main>
         </div>
     )
 
