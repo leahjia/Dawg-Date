e0229d2671d5aea95ee5308134e5a5184ad36b08
vinh <vinh.q.nguyen0122@gmail.com>
2022-11-23 19:52:53 -0800

    navbar styling

--- dawgdate/src/components/NavBar.js
+++ dawgdate/src/components/NavBar.js
@@ -32,8 +32,10 @@ export default function NavBar(props) {
     <nav>
       <div className="navbar">
         <Link to="/home">
-          <img src="img/logo.png" alt="logo" className="logo" />
-          <h1 className="name">DawgDate</h1>
+          {/* <div className="container"> */}
+            <img src="img/logo.png" alt="logo" className="logo" />
+            <h1 style={{display: 'inline', marginLeft: '1rem'}}className="name">DawgDate</h1>
+          {/* </div> */}
         </Link>
         {navLinks}
       </div>
