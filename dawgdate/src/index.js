b3e39e2acc828723bb627c6e66f2fa39fd14cb63
vinh <vinh.q.nguyen0122@gmail.com>
2022-12-06 23:42:45 -0800

    firebase setup

--- dawgdate/src/index.js
+++ dawgdate/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
 
 import App from './components/App.js';
 
@@ -9,8 +10,17 @@ import './style.css';
 
 import SAMPLE_PROFILES from './profile-data.json';
 
+const firebaseConfig = {
+	apiKey: "AIzaSyBJDHv8W0b6P2XuYFpJVPEe5X4YRy6Nb3E",
+	authDomain: "dawgdate-info340-project.firebaseapp.com",
+	projectId: "dawgdate-info340-project",
+	storageBucket: "dawgdate-info340-project.appspot.com",
+	messagingSenderId: "594425194974",
+	appId: "1:594425194974:web:4ef6be7714499a304d154f"
+};
 
-// const msgElem = <h1 id="DawgDate" className='title'>Title</h1>
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
