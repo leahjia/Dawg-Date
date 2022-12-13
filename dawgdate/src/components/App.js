1d3b4df7ffae4c92a496d9cf57236c572f3818f2
vinh <vinh.q.nguyen0122@gmail.com>
2022-12-12 17:09:17 -0800

    main app handle auth and database for users

--- dawgdate/src/components/App.js
+++ dawgdate/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route, Outlet, Navigate, useNavigate } from 'react-router-dom';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { deleteUser, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref, set as firebaseSet, get, onValue } from 'firebase/database';
 
 import LandingPage from './LandingPage.js';
 import HomePage from './HomePage.js';
@@ -9,42 +10,49 @@ import Footer from './Footer.js';
 import ConnectionsPage from './ConnectionsPage.js';
 import MyProfile from './MyProfile.js';
 import SignInPage from './SignInPage.js';
+import EditForm from './EditForm.js';
 
 import SAMPLE_PROFILES from '../profile-data.json';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 export default function App(props) {
   // hard coded user
-  const [currentUser, setCurrentUser] = useState(SAMPLE_PROFILES[0]) //default to null user
+  const [currentUser, setCurrentUser] = useState(null) //default to null user
   const [currentUserConnections, setCurrentUserConnections] = useState(null);
+  const [userList, setUserList] = useState(null);
 
   console.log("rendering app with", currentUser);
 
   const navigateTo = useNavigate(); //navigation hook
 
+  const db = getDatabase();
+  const auth = getAuth();
+
   useEffect(() => {
-    const auth = getAuth();
     onAuthStateChanged(auth, (firebaseUser) => {
       if (firebaseUser) {
-        console.log("logged in as", firebaseUser.displayName);
-        console.log("firebaseUser print: ", firebaseUser);
-        firebaseUser.UWNetId = firebaseUser.uid
-        firebaseUser.userName = firebaseUser.displayName;
-        firebaseUser.userImg = firebaseUser.photoURL || "/img/null.png"
-
-        setCurrentUser(firebaseUser);
+        const userRef = ref(db, 'userData/'+firebaseUser.uid)
+        get(userRef)
+          .then((response) => {
+            console.log(response.val());
+            setCurrentUser(response.val());
+          })
       } else {
         console.log("logged out");
-        setCurrentUser(SAMPLE_PROFILES[0]);
+        setCurrentUser(null);
       }
     })
+    const userListRef = ref(db, 'userData');
+    onValue(userListRef, ((snapshot) => {
+      console.log('running')
+      const userEntries = Object.entries(snapshot.val());
+      const userList = userEntries.map((entry) => {
+        return entry[1];
+      })
+      console.log(userList);
+      setUserList(userList);
+    }))
   }, []) //array is list of variables that will cause this to rerun if changed
-  const loginUser = (userObj) => {
-    console.log("logging in as", userObj.UWNetId);
-    setCurrentUser(userObj);
-    if (userObj.UWNetId !== null) {
-      navigateTo('/home');
-    }
-  }
 
 
   const handleConnection = function (connectee) {
@@ -61,13 +69,14 @@ export default function App(props) {
   return (
     <div>
       <Routes>
-        <Route index element={<LandingPage currentUser={currentUser} handleLoginCallback={setCurrentUser} handleConnectionsCallback={setCurrentUserConnections} />} />
-        <Route path="/signin" element={<SignInPage currentUser={currentUser} loginCallback={loginUser} />} />
+        <Route index element={<LandingPage currentUser={currentUser} />} />
+        <Route path="/signin" element={<SignInPage currentUser={currentUser} setCurrentUser={setCurrentUser}/>} />
         <Route element={<RequireAuth currentUser={currentUser} />}>
-          <Route path="/home" element={<HomePage profileData={props.profileData} currentUser={currentUser} currentUserConnections={currentUserConnections} handleConnectionCallback={handleConnection} />} />
+          <Route path="/home" element={<HomePage profileData={userList} currentUser={currentUser} currentUserConnections={currentUserConnections} handleConnectionCallback={handleConnection} />} />
           <Route path="/profile" element={<MyProfile currentUser={currentUser} />} />
-          <Route path="/connections" element={<ConnectionsPage profileData={props.profileData} currentUser={currentUser} currentUserConnections={currentUserConnections} handleConnectionCallback={handleConnection} />} />
-          <Route path="/user/:UWNetId" element={<OtherProfilePage profileData={props.profileData} currentUser={currentUser} />} />
+          <Route path="/connections" element={<ConnectionsPage profileData={userList} currentUser={currentUser} currentUserConnections={currentUserConnections} handleConnectionCallback={handleConnection} />} />
+          <Route path="/user/:UWNetId" element={<OtherProfilePage profileData={userList} currentUser={currentUser} />} />
+          <Route path="/edit" element={<EditForm />} />
         </Route>
       </Routes>
       <Footer />
@@ -77,7 +86,7 @@ export default function App(props) {
 }
 
 function RequireAuth(props) {
-  if (props.currentUser.UWNetId === null) {
+  if (props.currentUser === null) {
     return <Navigate to="/" />
   } else {
     return <Outlet />
